d065e279911ba7c229bd0a5a8e1a0fc1ab92cd83
fiffeek <fm394471@students.mimuw.edu.pl>
2019-04-21 23:42:56 +0200

    Update index.html, add in animations, update hover on menu items, add ts to improve animations stability

---
+++ ts/slide.ts
@@ -0,0 +1,28 @@
+let topSliders = document.getElementsByClassName("slide-top");
+const baseTimeout = 100;
+let timeout: number = 0;
+
+for (let slider of topSliders) {
+    setTimeout(() => {
+        slider.classList.add("slide-top-animated");
+    }, timeout);
+
+    for (let innerSlider of slider.children) {
+        setTimeout(() => {
+            innerSlider.classList.add("slide-bottom-animated");
+        }, timeout);
+    }
+
+    timeout += baseTimeout;
+}
+
+let menu = document.getElementsByClassName("index-menu");
+for (let single of menu) {
+    for (let child of single.children) {
+        child.addEventListener("mouseout", function(event) {
+            if (!child.classList.contains("swing-out")) {
+                child.classList.add("swing-out");
+            }
+        });
+    }
+}
